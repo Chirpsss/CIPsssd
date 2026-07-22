264995f5f29d5018dbd9d5fa7a31a13341b90ab6
Chirpsss <2844594329@QQ.COM>
2026-07-22 15:41:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express();
 app.use(cors({ origin: '*', methods: ['GET', 'POST'] }));
 app.use(express.json({ limit: '50mb' }));
 
-app.use('/generate/word', require('./routes/word'));
-app.use('/generate/pdf', require('./routes/pdf'));
-app.use('/generate/excel', require('./routes/excel'));
-app.use('/generate/ppt', require('./routes/ppt'));
-app.use('/search', require('./routes/search'));
+app.use('/generate/word', require('./word'));
+app.use('/generate/pdf', require('./pdf'));
+app.use('/generate/excel', require('./excel'));
+app.use('/generate/ppt', require('./ppt'));
+app.use('/search', require('./search'));
 
 app.get('/health', (req, res) => res.status(200).send('OK'));
 
